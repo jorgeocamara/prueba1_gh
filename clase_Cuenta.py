7c7eaca9e61d930eab55c4d5fe900b3f645cd57c
jorgeall <mistongo1@hotmail.com>
2022-06-26 18:00:29 -0300

    agregue clase cuenta

---
+++ clase_Cuenta.py
@@ -0,0 +1,19 @@
+
+class Cuenta():
+    '''
+    classdocs
+    '''
+    def __init__(self, saldoInicial):
+        '''
+        Constructor
+        '''
+        self.saldo=saldoInicial
+    def __str__ (self):
+        return "La cuenta bancaria tiene un saldo de " + str(self.saldo)
+    def depositar(self,monto):
+        self.saldo+=monto
+    def extraer(self,monto):
+        if self.saldo >= monto:
+            self.saldo-=monto
+        else:
+            print("No hay saldo disponible")
