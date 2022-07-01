ffe3d0c1e99dd7215c0064115ce70ed5f919956e
jorgeall <mistongo1@hotmail.com>
2022-06-30 22:29:56 -0300

    ggg

--- Template1_ejercicio.py
+++
@@ -1,7 +0,0 @@
-from string import Template
-t = Template('${village}folk send $$10 to $cause.')
-print(t.substitute(village='Nottingham', cause='the ditch fund'))
-t = Template('Return the $item to $owner.')
-#d = dict(item='unladen swallow')
-print(t.safe_substitute(item='unladen swallow', owner='Pepe'))
-#print(t.substitute(d))
